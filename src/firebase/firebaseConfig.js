c581fb085e485ff5b44b7928ea6d462a4312a599
Ikhlas Gadagin <ikhlashgadagin@gmail.com>
2025-03-07 12:44:42 +0530

    the process issue has been solved Vite

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,22 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDf9mX8DFDdQmQv8d2J5v7Bsm4ZEP9Ik4Y",
-    authDomain: "ramzanbase.firebaseapp.com",
-    projectId: "ramzanbase",
-    storageBucket: "ramzanbase.firebasestorage.app",
-    messagingSenderId: "723798272482",
-    appId: "1:723798272482:web:d571224bcccadc0ca131bb",
-    measurementId: "G-SHDLK9PDQG",
-    databaseURL: "https://ramzanbase-default-rtdb.firebaseio.com"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
